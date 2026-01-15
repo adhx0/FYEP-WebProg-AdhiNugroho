3355487f15357f26a2b039f1d920429276b4cad9
adhi <adhinugroho676@gmail.com>
2026-01-15 21:34:18 +0700

    update session-4 lagi boss

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -23,6 +23,6 @@ export async function fetchAPI<T>(
 }
 
 export function getImageUrl(path: string) {
-  if (path.startsWith("http")) return path; // artinya url nya sudah valid
-  return `${process.env.NEXT_PUBLIC_API_ROOT}${path}`;
+  if (path.startsWith("http")) return path;
+  return `${process.env.NEXT_PUBLIC_API_ROOT}/${path}`;
 }
