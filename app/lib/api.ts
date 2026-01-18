2f1f4e591e7323fad59421ce74cbd95bf63b39e4
adhi <adhinugroho676@gmail.com>
2026-01-19 00:25:17 +0700

    session-5

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -23,6 +23,13 @@ export async function fetchAPI<T>(
 }
 
 export function getImageUrl(path: string) {
-  if (path.startsWith("http")) return path;
-  return `${process.env.NEXT_PUBLIC_API_ROOT}/${path}`;
+  if (path.startsWith("http")) return path; // artinya url nya sudah valid
+  return `${process.env.NEXT_PUBLIC_API_ROOT}${path}`;
+}
+
+export function getAuthHeaders() {
+  const token = localStorage.getItem("token");
+  return {
+    Authorization: `Bearer ${token}`,
+  };
 }
